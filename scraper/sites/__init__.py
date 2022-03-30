9df03177853d2cacf8489355260b09fb452c2073
nicomoret7 <nicomoret7@gmail.com>
2022-03-30 09:42:51 +0200

    Added steam scraper

--- scraper/sites/__init__.py
+++ scraper/sites/__init__.py
@@ -3,8 +3,10 @@ from .g2a import scrapG2A
 from .gog import scrapGOG
 from .humbleBundle import scrapHB
 from .instantGaming import scrapIG
+from .steam import scrapSteam
 
 sites = {
+    "Steam":            scrapSteam,
     "Eneba":            scrapEneba,
     "G2A":              scrapG2A,
     "GOG":              scrapGOG,
---
+++ scraper/sites/steam.py
@@ -0,0 +1,31 @@
+import logging
+import requests
+from bs4 import BeautifulSoup
+from .funcs import parse_price, load_selenium
+
+
+def scrapSteam(name):
+
+    page = requests.get("https://store.steampowered.com/search/?term=%s" % name)
+    parser = BeautifulSoup(page.text, features="lxml")
+
+    # name = ''
+
+    '''
+    try:
+        file = open("steam.html", "w")
+        file.write(parser.text)
+        file.close()
+    except IOError as e:
+        logging.error("I/O Error", exc_info=True)
+    '''
+
+    try:
+        price = parser.find('div', attrs={'class': 'search_price'}).contents[-1].text
+        price = parse_price(price)
+        # name = parser.find('span', attrs={'class': 'YLosEL'}).text
+    except AttributeError:
+        logging.error("%s couldn't find a price" % scrapSteam.__name__)
+        price = "Not found"
+
+    return price
