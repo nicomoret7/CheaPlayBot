0fb16a13dfcadfd0991dcdd5dd19304506ec7d3e
nicomoret7 <nicomoret7@gmail.com>
2022-03-30 10:20:36 +0200

    Merge

--- scraper/sites/g2a.py
+++ scraper/sites/g2a.py
@@ -8,8 +8,8 @@ def scrapG2A(name):
     # Selenium (heavy on javascript or scrap-blocking page)
     driver = load_selenium()
 
-    driver.get("https://www.g2a.com/category/games-c189?f[device][0]=1118&f[drm]["
-               "0]=1&query=%s&sort=price-lowest-first&___currency=EUR" % name)
+    driver.get("https://www.g2a.com/category/games-c189?___currency=EUR&f[device][0]=1118&f[drm][0]=1&f[regions]["
+               "0]=8355&f[regions][1]=878&query=%s&sort=price-lowest-first" % name)
     parser = BeautifulSoup(driver.page_source, features="lxml")
     driver.close()
 
