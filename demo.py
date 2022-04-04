89df70104e9e5557215d5b5ff3f2e719d8872814
nicomoret7 <nicomoret7@gmail.com>
2022-04-04 10:31:22 +0200

    Added demo.png

---
+++ demo.py
@@ -0,0 +1,5 @@
+from scraper.scraper import scrap
+
+
+gameName = input("Insert game name: ")
+scrap(gameName)
--- scraper/demo.py
+++
@@ -1,7 +0,0 @@
-from .sites import sites
-
-gameName = input("Insert game name: ")
-
-for site in sites:
-    print("Scanning %s" % site, end='\r')
-    print("%s: %s" % (site, sites[site](gameName)), end='\n')
